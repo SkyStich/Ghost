ced2eaed685824cb5171c1c649ce0887a08b2b13
Stich <SkyandSmurf@gmail.com>
2021-08-25 22:53:35 +0300

    Create base item with which to interact

---
+++ Source/Ghost/Private/Actors/Items/Base/BaseItem.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Items/Base/BaseItem.h"
+
+// Sets default values
+ABaseItem::ABaseItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	bReplicates = true;
+	NetUpdateFrequency = 10.f;
+	NetCullDistanceSquared = 5000.f;
+}
+
+void ABaseItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Ghost/Public/Actors/Items/Base/BaseItem.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseItem.generated.h"
+
+/** Base item with which to interact */
+UCLASS()
+class GHOST_API ABaseItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
