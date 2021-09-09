f1dbf651c7a1292ed2265547243a589ae025897e
Stich <SkyandSmurf@gmail.com>
2021-09-10 01:05:58 +0300

    Add blackboard keys namespace

---
+++ Source/Ghost/Private/BlackboardNamespace/BlackboardKeys.h
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Runtime/Core/Public/UObject/NameTypes.h"
+#include "Runtime/Core/Public/Containers/UnrealString.h"
+
+namespace bb_keys
+{
+	/** the point to which the ghost is streaming */
+	TCHAR const * const Target_Location = TEXT("TargetLocation");
+}
